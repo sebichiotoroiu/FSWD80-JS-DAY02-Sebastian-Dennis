cca1e614a1e815040c077d0e1a172dd14608c6d8
Sebi <sebichiotoroiu@yahoo.com>
2019-09-23 14:29:38 +0200

    basic6.Error.html

--- basic5.Area.js
+++ basic5.Area.js
@@ -1,6 +1,16 @@
-function converts(degrees){
-	var degrees = prompt("Please give me the degrees");
-	var degrees_convert = degrees *(Math.PI/180);
-	document.write("Your result= " + degrees_convert + " radians");
+function area(width, height){
+	var width = prompt("Please give me the width");
+	var height = prompt("please give me the height");
+	var area = width * height;
+	document.write("Area= " + width + "*" + height + "=" + area);
 }
 
+function volum(width, height, depth){
+	var width = prompt("Please give me the width");
+	var height = prompt("please give me the height");
+	var depth = prompt("please give me the depth");
+	var area = width * height;
+	var volum = width * height * depth;
+	document.write("Area= " + width + "*" + height+ "=" + area);
+	document.write("Volum= " + width + "*" + height + "*" + depth + "=" + volum);
+}
---
+++ intermediate/Int2Sebi.js
@@ -0,0 +1,10 @@
+
+
+function averageGrade(){
+	var math = prompt("Please give me the math note");
+	var physics = prompt("Please give me the physics note");
+	var english = prompt("Please give me the physics note");
+	var result = parseInt(math) + parseInt(physics) + parseInt(english);
+	document.write("Sum:" + result);
+
+}
